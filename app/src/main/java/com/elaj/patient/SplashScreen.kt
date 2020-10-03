80d9178a380c7e97dbef92517754689fbb8befce
ameeraqel <ameer_agel@hotmail.com>
2020-10-03 21:56:48 +0300

    3-10-2020

--- app/src/main/java/com/elaj/patient/SplashScreen.kt
+++ app/src/main/java/com/elaj/patient/SplashScreen.kt
@@ -92,8 +92,8 @@ class SplashScreen : ActivityBase() {
                 val intent = Intent(
                     getActiviy(),
 //                    LoginActivity::class.java
-//                    WelcomeActivity::class.java
-                    MainActivityBottomNav::class.java
+                    WelcomeActivity::class.java
+//                    MainActivityBottomNav::class.java
                 )
                 intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TASK or Intent.FLAG_ACTIVITY_NEW_TASK
                 startActivity(intent)
